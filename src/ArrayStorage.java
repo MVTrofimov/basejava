9f5a646c174b762a79a6dd65e48e87ecbc1d0148
MVTrofimov <matvey.trofimov.03@inbox.ru>
2022-07-27 17:56:44 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,71 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++){
+            if (storage[i] != null){
+                storage[i] = null;
+            } else{
+                break;
+            }
+        }
     }
 
     void save(Resume r) {
+        if (size() < storage.length){
+            storage[size()] = r;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int flag = 0;
+        Resume return_res = null;
+        for (Resume resume : storage) {
+            if (resume != null && Objects.equals(resume.toString(), uuid)) {
+                flag = 1;
+                return_res = resume;
+                break;
+            }
+        }
+        if (flag == 1) {
+            return return_res;
+        } else{
+            return null;
+        }
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++){
+            int flag = 0;
+            if (storage[i] != null && Objects.equals(storage[i].toString(), uuid)){
+                flag = 1;
+            }
+            if (flag == 1){
+                while (storage[i+1] != null){
+                    storage[i] = storage[i+1];
+                    i += 1;
+                }
+                storage[i] = null;
+            }
+
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] return_arr = new Resume[size()];
+        for (int i = 0; i < size(); i++){
+            return_arr[i] = storage[i];
+        }
+        return return_arr;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        while (storage[count] != null){
+            count += 1;
+        }
+        return count;
     }
 }
